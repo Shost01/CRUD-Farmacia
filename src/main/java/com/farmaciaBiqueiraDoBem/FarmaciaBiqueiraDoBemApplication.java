490a1012b33452e8b1fc29e22fa6bdd831eacc3b
Nathan <ribeiro.nathan2002@gmail.com>
2025-01-27 09:30:17 -0300

    Configurando properties

---
+++ src/main/java/com/farmaciaBiqueiraDoBem/FarmaciaBiqueiraDoBemApplication.java
@@ -0,0 +1,13 @@
+package com.farmaciaBiqueiraDoBem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaBiqueiraDoBemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaBiqueiraDoBemApplication.class, args);
+	}
+
+}
